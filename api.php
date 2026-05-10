029fd5aadd098c957a44ad9b39a91d42367007aa
Erik <ej.erik.jimenez@gmail.com>
2026-05-10 15:04:23 -0500

    data

--- api.php
+++ api.php
@@ -1,10 +1,12 @@
 <?php
 header('Content-Type: application/json; charset=UTF-8');
 header('HTTP/1.1 200 OK');
-$url=$_SERVER['REQUEST_URI'];
+$url = $_SERVER['REQUEST_URI'];
+$data = explode("/",trim($url,"/"));
+$method = $_SERVER['REQUEST_METHOD'];
 
-$resArr=array("status"=>"Success", "message" => "hello world!","data"=>$url);
+$resArr = array("status"=>"Success", "message" => "hello world!","data"=>$data, "method"=>$method);
 
-$res=json_encode($resArr);
+$res = json_encode($resArr);
 echo $res;
 ?>

24d3a60b687f63392879e108a0f44cc7e9d2b842
Erik <ej.erik.jimenez@gmail.com>
2026-05-10 16:07:00 -0500

    error checking

--- api.php
+++ api.php
@@ -9,36 +9,71 @@ $endpoint = $reqPayload[1];
 #endpoints
 switch ($endpoint) {
    case 'list_manufacturers':
+      if($method != "GET") {
+      
+      }
       include('services/manufacturer_service/list_manufacturers.php');
       break;
    case 'list_device_types':
+      if($method != "GET") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'add_device_type':
+      if($method != "POST") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'add_manufacturer':
+      if($method != "POST") {
+         sendError("Invalid method", $method, 401); 
+      } 
       break;
    case 'add_equipment':
       break;
    case 'search_by_device_type':
+      if($method != "GET") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'search_by_manufacturer':
+      if($method != "GET") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'search_by_device_type':
+      if($method != "GET") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'search_by_serial_number':
+      if($method != "GET") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'modify_equipment_by_id':
+      if($method != "PATCH") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'modify_device_type_by_id':
+      if($method != "PATCH") {
+         sendError("Invalid method", $method, 401); 
+      }
       break;
    case 'modify_manufacturer_by_id':
+      if($method != "PATCH") {
+         sendError("Invalid method", $method, 401); 
+      }
+      break;
    default:
-      sendError("Requested Endpoint Does Not Exist", $method); 
+      sendError("Requested Endpoint Does Not Exist", $method, 404); 
       break;
 }
 die();
 
-function sendError($message, $method) {
+function sendError($message, $method, $code) {
+   http_response_code($code);
    $resArr = array("status"=>"Error", "message" => $message,"data"=>"", "method"=>$method);
    $res = json_encode($resArr);
    echo $res;

b75b6f07742fea5c993a6612208eb25d0becbe88
Erik <ej.erik.jimenez@gmail.com>
2026-05-10 15:39:39 -0500

    router complete

--- api.php
+++ api.php
@@ -2,11 +2,45 @@
 header('Content-Type: application/json; charset=UTF-8');
 header('HTTP/1.1 200 OK');
 $url = $_SERVER['REQUEST_URI'];
-$data = explode("/",trim($url,"/"));
+$reqPayload = explode("/",trim($url,"/"));
 $method = $_SERVER['REQUEST_METHOD'];
+$endpoint = $reqPayload[1];
 
-$resArr = array("status"=>"Success", "message" => "hello world!","data"=>$data, "method"=>$method);
+#endpoints
+switch ($endpoint) {
+   case 'list_manufacturers':
+      include('services/manufacturer_service/list_manufacturers.php');
+      break;
+   case 'list_device_types':
+      break;
+   case 'add_device_type':
+      break;
+   case 'add_manufacturer':
+      break;
+   case 'add_equipment':
+      break;
+   case 'search_by_device_type':
+      break;
+   case 'search_by_manufacturer':
+      break;
+   case 'search_by_device_type':
+      break;
+   case 'search_by_serial_number':
+      break;
+   case 'modify_equipment_by_id':
+      break;
+   case 'modify_device_type_by_id':
+      break;
+   case 'modify_manufacturer_by_id':
+   default:
+      sendError("Requested Endpoint Does Not Exist", $method); 
+      break;
+}
+die();
 
-$res = json_encode($resArr);
-echo $res;
+function sendError($message, $method) {
+   $resArr = array("status"=>"Error", "message" => $message,"data"=>"", "method"=>$method);
+   $res = json_encode($resArr);
+   echo $res;
+}
 ?>

8c05d3384aa76e49128e0b80d29c507ceba3b5dd
Erik <ej.erik.jimenez@gmail.com>
2026-05-10 23:36:39 -0500

    fixed all messages

--- services/status_service.php
+++ services/status_service.php
@@ -4,6 +4,6 @@ include_once('responce_service.php');
 function getAllStatuses() {
    $sql = "SELECT * FROM `status`";
    $data = query($sql, 'GET');
-   sendSuccess("Found All Device Types", 'GET', 200, $data);
+   sendSuccess("Found All Statuses", 'GET', 200, $data);
 }
 ?>

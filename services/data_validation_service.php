38739cb6d5dd818927412b647e7c51a61fb46f4c
Erik <ej.erik.jimenez@gmail.com>
2026-05-11 01:34:59 -0500

    part and validate serial number

---
+++ services/data_validation_service.php
@@ -0,0 +1,30 @@
+<?php
+
+function partSerialNumber(string $fullSerialNumber, &$prefix, &$delimeter, &$body) : void {
+   if($fullSerialNumber == null) {
+      return;
+   }
+
+   $prefix = substr($fullSerialNumber, 0,2);
+   $delimeter = $fullSerialNumber[2];
+   $body = substr($fullSerialNumber, 3);
+}
+
+function validateSerialNumber(&$prefix, &$body, $serialNumber) : bool {
+   $delimeter = "";
+   partSerialNumber($serialNumber, $prefix, $delimeter, $body);
+
+   //incorect size
+   if(strlen($body) != 64) {
+      return true;
+   }
+
+   //incorect delimiter
+   if($delimeter != '-') {
+      return true;
+   }
+
+   return false;
+}
+
+?>

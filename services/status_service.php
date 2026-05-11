9c82a04655023451afcd446afa8b123272d44ca9
Erik <ej.erik.jimenez@gmail.com>
2026-05-10 23:34:13 -0500

    fixed sql

--- services/status_service.php
+++ services/status_service.php
@@ -2,7 +2,7 @@
 include_once('database_service.php'); 
 include_once('responce_service.php');
 function getAllStatuses() {
-   $sql = "SELECT * FROM `statuses`";
+   $sql = "SELECT * FROM `status`";
    $data = query($sql, 'GET');
    sendSuccess("Found All Device Types", 'GET', 200, $data);
 }

02fdcf30e19addbcd3f0769d3f4ab2bac2bde65c
Erik <ej.erik.jimenez@gmail.com>
2026-05-10 21:55:12 -0500

    database service

---
+++ services/database_service.php
@@ -0,0 +1,36 @@
+<?php
+include_once('responce_service.php');
+
+function db_connect($db, $method){    
+   $hostname = "localhost";
+   $username = "";
+   $password = "";
+   
+   $dblink = new mysqli($hostname, $username, $password, $db);
+   
+   if (mysqli_connect_error())
+   {
+     sendError("Database Connection Failed", 500, $method); 
+     die();
+   }
+   
+   return $dblink;
+}
+
+function query($sql, $method) {
+   $dblink = db_connect("equipment", $method);
+   $result = $dblink->query($sql) or queryFail($method);
+   $res = $result->fetch_all(MYSQL_ASSOC);
+   
+   #free mysql connection
+   $result->free(); 
+   $dblink->close();
+
+   return $res;
+}
+
+function queryFail($method) {
+   sendError("Invalid SQL", 500, $method);
+   die(); 
+}
+?>

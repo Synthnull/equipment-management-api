323a7219d90a0b801e8b2095a21c1b767adb7334
Erik <ej.erik.jimenez@gmail.com>
2026-05-10 23:29:22 -0500

    fixed bug

--- services/manufacturer_service.php
+++ services/manufacturer_service.php
@@ -3,7 +3,7 @@ include_once('database_service.php');
 include_once('responce_service.php');
 function getAllManufacturers() {
    $sql = "SELECT * FROM `manufacturers`";
-   $data = json_encode(query($sql, 'GET'));
+   $data = query($sql, 'GET');
    sendSuccess("Found All Manufacturers", 'GET', 200, $data);
 }
 ?>
